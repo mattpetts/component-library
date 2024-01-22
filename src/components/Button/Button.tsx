ad1cf965aabf9cf4148326b01641d091604d2fdb
mattpetts <mattpetts23@gmail.com>
2024-01-22 15:00:54 +0000

    add a card compoennt

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-
 import style from './Button.module.css'
 
-export interface ButtonProps {
+interface ButtonProps {
   label: string
-  handleClick: () => void
 }
 
 export const Button = (props: ButtonProps): JSX.Element => {
-  return <button className={style.button} onClick={() => { props.handleClick() }}>{props.label}</button>
+  return <button className={style.button}>{props.label}</button>
 }
